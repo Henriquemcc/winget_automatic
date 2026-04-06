410cf660fe9881574e3ce32f38afb8446fece89b
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-04-06 08:14:43 -0300

    Fixing service installation issue

--- installer/Build.ps1
+++ installer/Build.ps1
@@ -6,15 +6,15 @@ param(
 # Removes the prefix 'v' from the tag name (eg: v1.2.3 -> 1.2.3)
 $ProductVersion = $Version.TrimStart('v')
 
-# Building dotnet service
-dotnet build --self-contained --configuration Release --arch x86 --os win ..
-dotnet build --self-contained --configuration Release --arch x64 --os win ..
+# Publishing dotnet service as a single file to ensure all dependencies are inside the EXE
+dotnet publish -c Release -r win-x86 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true ..
+dotnet publish -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true ..
 
 # Preparing installer files
 $installerStage = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition), "installer-stage")
 New-Item -Path $installerStage -ItemType Directory -Force
-Copy-Item -Path ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "bin", "Release", "net10.0", "win-x86", "winget_automatic.exe")) -Destination ([System.IO.Path]::Combine($installerStage, "winget_automatic_x86.exe"))
-Copy-Item -Path ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "bin", "Release", "net10.0", "win-x64", "winget_automatic.exe")) -Destination ([System.IO.Path]::Combine($installerStage, "winget_automatic_x64.exe"))
+Copy-Item -Path ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "bin", "Release", "net10.0", "win-x86", "publish", "winget_automatic.exe")) -Destination ([System.IO.Path]::Combine($installerStage, "winget_automatic_x86.exe"))
+Copy-Item -Path ([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)), "bin", "Release", "net10.0", "win-x64", "publish", "winget_automatic.exe")) -Destination ([System.IO.Path]::Combine($installerStage, "winget_automatic_x64.exe"))
 
 # Building MSI Package
 dotnet build /p:ProductVersion=$ProductVersion /p:DefineConstants="ProductVersion=$ProductVersion"
